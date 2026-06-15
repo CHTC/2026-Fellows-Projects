bc9af860860a88de9b47286e93f0101938c1ba04
snikithasiddavatam <snikithasiddavatam@gmail.com>
2026-06-15 09:46:18 -0500

    docstring

--- snikithasiddavatam/monte_carlo_pi/utils.py
+++ snikithasiddavatam/monte_carlo_pi/utils.py
@@ -1,7 +1,9 @@
 import os
 
-
 def get_run_folders(BASE_DIR = "mc_runs", with_results_csv = True):
+    """
+    Get the list of run folders in the BASE_DIR. If with_results_csv is True, only return those folders that contain a results.csv file.
+    """
     return sorted([
         d for d in os.listdir(BASE_DIR)
         if os.path.isdir(os.path.join(BASE_DIR, d)) and d.startswith("run_")
